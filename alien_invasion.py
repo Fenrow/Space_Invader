b6923dd73b632a02cd936b95e0f62bbd60b831fd
Fenrow <45560852+Fenrow@users.noreply.github.com>
2018-12-18 14:44:57 +0100

    v0.1 +Klasa Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,15 +2,16 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 def run_game():
     #Inicjalizacja gry i utworzenie obiektu ekranu
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption('Inwazja Obcych')
 
-    #zdefiniowanie koloru tła
-    bg_color = (230, 230, 230)
-    
     #Rozpoczęcie pętli głównej gry
     while True:
 
@@ -20,7 +21,7 @@ def run_game():
                 sys.exit()
 
         #Odświeżenie ekranu w trakcie każdej iteracji pętli
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         #Wyświetlanie ostatnio zmodyfikowanego ekranu
         pygame.display.flip()
